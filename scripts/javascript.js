31f4246f9b09e68de6c4aeede0b128096e5235e7
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2023-10-15 16:48:35 +0200

    Added buttons, functions and lets

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,15 +1,24 @@
-function add() {
-    
+let firstNumber;
+let secondNumber;
+let operator;
+let display;
+function add(firstNumber, secondNumber) {
+    return Number(firstNumber) + Number(secondNumber);
 };
 
-function subtract() {
+function subtract(firstNumber, secondNumber) {
+    return Number(firstNumber) - Number(secondNumber);
+};
+
+function multiply(firstNumber, secondNumber) {
+    return Number(firstNumber) * Number(secondNumber);
+};
+
+function divide(firstNumber, secondNumber) {
+    return Number(firstNumber) / Number(secondNumber);
+};
+
+function operate(firstNumber, secondNumber) {
 
 };
 
-function multiply() {
-
-};
-
-function divide() {
-
-};
