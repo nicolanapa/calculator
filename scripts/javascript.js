b9e61e644904f25acc3ba26280f8e45bafdd4f75
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2023-10-15 17:06:37 +0200

    Added main functions

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -2,6 +2,47 @@ let firstNumber;
 let secondNumber;
 let operator;
 let display;
+display = document.querySelector("#display");
+let zero = document.querySelector("#0");
+zero.addEventListener("click", () => {
+
+});
+let one = document.querySelector("#1");
+one.addEventListener("click", () => {
+    
+});
+let two = document.querySelector("#2");
+two.addEventListener("click", () => {
+    
+});
+let three = document.querySelector("#3");
+three.addEventListener("click", () => {
+    
+});
+let four = document.querySelector("#4");
+four.addEventListener("click", () => {
+    
+});
+let five = document.querySelector("#5");
+five.addEventListener("click", () => {
+    
+});
+let six = document.querySelector("#6");
+six.addEventListener("click", () => {
+    
+});
+let seven = document.querySelector("#7");
+seven.addEventListener("click", () => {
+    
+});
+let eight = document.querySelector("#8");
+o.addEventListener("click", () => {
+    
+});
+let nine = document.querySelector("#9");
+nine.addEventListener("click", () => {
+    
+});
 function add(firstNumber, secondNumber) {
     return Number(firstNumber) + Number(secondNumber);
 };
